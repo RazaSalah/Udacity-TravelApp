2892f9baaded7f267bc2f2071d8cf1d44b680874
Razan Alsubaie <79215594+RazaSalah@users.noreply.github.com>
2024-01-01 00:26:20 +0300

    set webpack

---
+++ webpack.prod.js
@@ -0,0 +1,47 @@
+const common = require("./webpack.common.js");
+const { merge } = require("webpack-merge");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+path = require("path");
+const WorkboxPlugin = require("workbox-webpack-plugin");
+
+module.exports = merge(common, {
+  mode: "production",
+  devtool: "hidden-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    libraryTarget: "var",
+    library: "Client",
+    clean: true,
+  },
+  optimization: {
+    minimizer: [
+      // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+      // `...`,
+      new CssMinimizerPlugin(),
+    ],
+    minimize: true,
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "style.[contenthash].css",
+    }),
+
+    new WorkboxPlugin.GenerateSW({
+      // these options encourage the ServiceWorkers to get in there fast
+      // and not allow any straggling "old" SWs to hang around
+      clientsClaim: true,
+      skipWaiting: true,
+    }),
+  ],
+});
